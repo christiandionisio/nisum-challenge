252ab87adf28e0c6e5463bab7205f1379ba25b12
Christian Dionisio <christiandionisio9432@gmail.com>
2023-06-17 14:29:32 -0500

    initial commit

---
+++ src/test/java/com/example/nisumchallenge/NisumChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nisumchallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NisumChallengeApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
