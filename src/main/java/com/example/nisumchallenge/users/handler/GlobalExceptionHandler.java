85624e5b4169ef7ae7d39fbe0c63b923bcb04c6b
Christian Dionisio <christiandionisio9432@gmail.com>
2023-06-17 19:55:11 -0500

    Validations exception added

--- src/main/java/com/example/nisumchallenge/users/handler/GlobalExceptionHandler.java
+++ src/main/java/com/example/nisumchallenge/users/handler/GlobalExceptionHandler.java
@@ -3,6 +3,8 @@ package com.example.nisumchallenge.users.handler;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -14,11 +16,25 @@ import java.util.Map;
 public class GlobalExceptionHandler {
 
   @ExceptionHandler(DataIntegrityViolationException.class)
-  public ResponseEntity<Map<String, String>> handleValidationExceptions(DataIntegrityViolationException ex) {
+  public ResponseEntity<Map<String, String>> handleDataIntegrityViolationException(DataIntegrityViolationException ex) {
     Map<String, String> errors = new HashMap<>();
     errors.put("email", "El correo ya registrado.");
     log.info(errors.toString());
     return ResponseEntity.badRequest().body(errors);
   }
+
+  @ExceptionHandler(MethodArgumentNotValidException.class)
+  public ResponseEntity<Map<String, String>> handleValidationExceptions(MethodArgumentNotValidException ex) {
+    Map<String, String> errors = new HashMap<>();
+    ex.getBindingResult().getAllErrors().forEach(error -> {
+      String fieldName = ((FieldError) error).getField();
+      String errorMessage = error.getDefaultMessage();
+      errors.put(fieldName, errorMessage);
+    });
+    log.info(errors.toString());
+    return ResponseEntity.badRequest().body(errors);
+  }
+
+
 
 }
